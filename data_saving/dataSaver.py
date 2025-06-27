758f043a8fe0c50781adf2f920ba81c6287e4d50
pedramjlo <pedram_sif@tuta.io>
2025-06-27 13:43:45 +0330

    added data saver class

---
+++ data_saving/dataSaver.py
@@ -0,0 +1,24 @@
+import os
+import logging
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+)
+
+
+class DataSaver:
+    def save_clean_data(self, df, filename, output_dir="dataset/cleaned_data"):
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        file_path = os.path.join(output_dir, filename)
+
+        try:
+            df.to_csv(file_path, index=False)
+            logging.info(f"Cleaned data saved to {file_path}")
+        except Exception as e:
+            logging.error(f"Failed to save cleaned data: {e}")
+        
+        return file_path
