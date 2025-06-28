ba31f6966d36fd7030e0bb5dcc0a4599e217f19a
pedramjlo <pedram_sif@tuta.io>
2025-06-28 14:14:27 +0330

    added gender spending per city

--- descriptive_analysis/sql/main.sql
+++ descriptive_analysis/sql/main.sql
@@ -6,10 +6,11 @@ SELECT * FROM market_sales;
 -- TOTAL REVENUE BY CITY
 SELECT 
     city,
-    (unit_price * quantity) + calculated_tax as total_revenue
+    SUM((unit_price * quantity) + calculated_tax) AS total_revenue
 FROM market_sales
 GROUP BY  city
 ORDER BY total_revenue DESC;
+
 
 
 -- REVENUE BY CATEGORY PER CITY
@@ -27,3 +28,20 @@ SELECT
     total_revenue
 FROM REVENUE_PER_CATEGORY
 ORDER BY city, product_line, total_revenue DESC;
+
+
+-- MALE AND FEMALE TOTAL SPENDING BY CITY
+WITH REVENUE_BY_GENDER AS (
+    SELECT
+        city,
+        gender,
+        SUM((unit_price * quantity) + calculated_tax) AS total_spending
+    FROM market_sales
+    GROUP BY city, gender
+)
+SELECT 
+    city,
+    gender,
+    total_spending
+FROM REVENUE_BY_GENDER
+ORDER BY city, gender, total_spending;
