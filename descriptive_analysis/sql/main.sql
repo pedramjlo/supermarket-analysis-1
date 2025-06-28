b6071d255e4028ecd21f38fc1e99fe2bf24d309b
pedramjlo <pedram_sif@tuta.io>
2025-06-28 14:08:40 +0330

    added revenue by city and category revenue by city

--- descriptive_analysis/sql/main.sql
+++ descriptive_analysis/sql/main.sql
@@ -13,3 +13,17 @@ ORDER BY total_revenue DESC;
 
 
 -- REVENUE BY CATEGORY PER CITY
+WITH REVENUE_PER_CATEGORY AS (
+    SELECT
+        city,
+        product_line,
+        SUM((unit_price * quantity) + calculated_tax) AS total_revenue
+    FROM market_sales
+    GROUP BY city, product_line
+)
+SELECT 
+    city,
+    product_line,
+    total_revenue
+FROM REVENUE_PER_CATEGORY
+ORDER BY city, product_line, total_revenue DESC;
